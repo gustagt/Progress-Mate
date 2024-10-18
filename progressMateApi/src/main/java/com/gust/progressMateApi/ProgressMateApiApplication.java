dc955a03fc41c6b1c85ff8ed87bf37d5eaa638f4
Gustavo Augusto <gust.aug.2ga@gmail.com>
2024-10-18 16:57:45 -0300

    Base do projeto

---
+++ progressMateApi/src/main/java/com/gust/progressMateApi/ProgressMateApiApplication.java
@@ -0,0 +1,13 @@
+package com.gust.ProgressMateApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressMateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressMateApiApplication.class, args);
+	}
+
+}
---
+++ progressMateApi/src/test/java/com/gust/progressMateApi/ProgressMateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gust.ProgressMateApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressMateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
